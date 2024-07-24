eaf275c154ae3eaf93ac05195085b3c1b0f9034e
Djimra NGARLEITA <dngarleita@gmail.com>
2024-07-25 02:31:53 +0300

    fix: log before returning response

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,8 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => ({
+  promise.then(() => {
+    console.log('Got a response from the API');
+  }).then(() => ({
     status: 200,
     body: 'Success',
-  })).then(() => {
-    console.log('Got a response from the API');
-  }).catch(() => new Error());
+  })).catch(() => new Error());
 }
