f118d1962bc5d72ffa7ffee98f08dba91e5f6e83
Djimra NGARLEITA <dngarleita@gmail.com>
2024-07-25 02:33:23 +0300

    fix

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
+  return promise.then(() => {
     console.log('Got a response from the API');
   }).then(() => ({
     status: 200,
