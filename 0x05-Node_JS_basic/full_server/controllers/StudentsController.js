340fc0edd8b25a789714b254f85909397bf352bb
Djimra NGARLEITA <dngarleita@gmail.com>
2024-09-19 03:46:21 +0300

    fix(#8): Fix server error handling

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -5,31 +5,31 @@ export default class StudentsController {
   static getAllStudents(req, res) {
     const db = path.resolve(process.argv[2]);
     res.set('Content-Type', 'text/plain');
-    res.write('This is the list of our students');
     readDatabase(db).then((data) => {
       const keys = Object.keys(data).sort();
+      res.write('This is the list of our students');
       for (const key of keys) {
         const list = data[key];
         res.write(`\nNumber of students in ${key}: ${list.length}. List: ${list.join(', ')}`);
       }
-      res.status(200).end();
+      res.status(200).send();
     }).catch((error) => {
-      res.status(500).send(error.message);
+      res.status(500).send(`This is the list of our students\n${error.message}`);
     });
   }
 
   static getAllStudentsByMajor(req, res) {
     const db = path.resolve(process.argv[2]);
     const { major } = req.params;
-    console.log('Database file path:', db);
     if (major !== 'CS' && major !== 'SWE') {
       res.status(500).send('Major parameter must be CS or SWE');
+    } else {
+      readDatabase(db).then((data) => {
+        const list = data[major];
+        res.status(200).send(`List: ${list.join(', ')}`);
+      }).catch((error) => {
+        res.status(500).send(error.message);
+      });
     }
-    readDatabase(db).then((data) => {
-      const list = data[major];
-      res.status(200).send(`List: ${list.join(', ')}`);
-    }).catch((error) => {
-      res.status(500).send(error.message);
-    });
   }
 }
