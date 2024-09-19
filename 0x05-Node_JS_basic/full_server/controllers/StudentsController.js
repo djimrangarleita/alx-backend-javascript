c3b52f53136b600c5cb2ab89a5dbb80aee7fd9b7
Djimra NGARLEITA <dngarleita@gmail.com>
2024-09-19 03:52:54 +0300

    fix(#8): Don't print first line if db read fails

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -14,7 +14,7 @@ export default class StudentsController {
       }
       res.status(200).send();
     }).catch((error) => {
-      res.status(500).send(`This is the list of our students\n${error.message}`);
+      res.status(500).send(error.message);
     });
   }
 
