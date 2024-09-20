12fe98968dd2a7d70e63182a690de9177c5a11a5
Djimra NGARLEITA <dngarleita@gmail.com>
2024-09-21 02:58:57 +0300

    chore(#4): Stub calculateNumber to always return 10

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,22 @@
+/* eslint-disable jest/valid-expect */
+/* eslint-disable jest/prefer-expect-assertions */
+/* eslint-disable jest/expect-expect */
+/* eslint-disable no-unused-expressions */
+
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('stubCalculateNumberOfUtils', () => {
+  it('stub the calculateNumber function in Utils', () => {
+    const utilsCalculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+    const consoleSpy = sinon.spy(console, 'log');
+    sendPaymentRequestToApi(100, 20);
+    expect(utilsCalculateNumberStub.calledWith('SUM', 100, 20)).true;
+    expect(consoleSpy.calledWith('The total is: 10')).true;
+    utilsCalculateNumberStub.restore();
+    consoleSpy.restore();
+  });
+});
