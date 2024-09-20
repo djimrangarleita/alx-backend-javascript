858a20744ab068b3ef36985e876968412d1f86df
Djimra NGARLEITA <dngarleita@gmail.com>
2024-09-20 04:45:40 +0300

    chore(#3): Use sinon to spy function call

---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,22 @@
+/* eslint-disable jest/valid-expect */
+/* eslint-disable jest/prefer-expect-assertions */
+/* eslint-disable jest/expect-expect */
+/* eslint-disable no-unused-expressions */
+
+const { expect } = require('chai');
+const sinon = require('sinon');
+
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('validateUsageOfUtils', () => {
+  it('validate the usage of the Utils function', () => {
+    const utilsCalculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+    const consoleSpy = sinon.spy(console, 'log');
+    sendPaymentRequestToApi(100, 20);
+    expect(utilsCalculateNumberSpy.calledWith('SUM', 100, 20)).true;
+    expect(consoleSpy.calledWith('The total is: 120')).true;
+    utilsCalculateNumberSpy.restore();
+    consoleSpy.restore();
+  });
+});
